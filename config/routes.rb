14a54ffe3bf0af22478a94a1194b7244ab6fbd6a
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-24 13:17:17 -0700

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "pages#index"
   get "/pages" => "pages#index"
-  
+
   resources :movies
   get "/random" => "movies#random"
+
+  post "/users" => "users#create"
 end
